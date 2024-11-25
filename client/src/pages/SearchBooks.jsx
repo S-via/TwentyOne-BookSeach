7e5209380cb6c58a614a9770cd5c1b987f3a19e0
S-via <168160925+S-via@users.noreply.github.com>
2024-11-25 00:05:07 -0800

    added logic for searchBook.jsx , handleSaveBook

--- client/src/pages/SearchBooks.jsx
+++ client/src/pages/SearchBooks.jsx
@@ -9,8 +9,13 @@ import {
 } from 'react-bootstrap';
 
 import Auth from '../utils/auth';
-import { saveBook, searchGoogleBooks } from '../utils/API';
+import { searchGoogleBooks } from '../utils/API';
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
+
+ 
+import { useMutation} from '@apollo/client';
+import { SAVE_BOOK} from '../utils/mutations';  
+
 
 const SearchBooks = () => {
   // create state for holding returned google api data
@@ -61,6 +66,8 @@ const SearchBooks = () => {
 
   // create function to handle saving a book to our database
   const handleSaveBook = async (bookId) => {
+    // added below line 70 ,82,83,86
+    const [saveBook] = useMutation(SAVE_BOOK);
     // find the book in `searchedBooks` state by the matching id
     const bookToSave = searchedBooks.find((book) => book.bookId === bookId);
 
@@ -72,9 +79,11 @@ const SearchBooks = () => {
     }
 
     try {
-      const response = await saveBook(bookToSave, token);
+      const {data} = await saveBook({
+        variables:{bookData:bookToSave},
+    });
 
-      if (!response.ok) {
+      if (!data) {
         throw new Error('something went wrong!');
       }
 
